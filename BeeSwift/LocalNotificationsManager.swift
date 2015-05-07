70ad6d77c6e5969c6aab374ad56d4ded13013d0a
Andy Brett <andy@andybrett.com>
2015-05-07 00:16:06 -0700

    settings

--- BeeSwift/LocalNotificationsManager.swift
+++ BeeSwift/LocalNotificationsManager.swift
@@ -12,6 +12,10 @@ class LocalNotificationsManager {
     
     private var hourKey = "hour"
     private var minuteKey = "minute"
+    private var notificationsOnKey = "notificationsOn"
+    
+    private var defaultHour = 21
+    private var defaultMinute = 0
     
     class var sharedManager :LocalNotificationsManager {
         struct Manager {
@@ -20,24 +24,59 @@ class LocalNotificationsManager {
         return Manager.sharedManager
     }
     
+    func humanizedReminderTime() -> String {
+        let components = NSDateComponents()
+        components.hour = LocalNotificationsManager.sharedManager.reminderTimeHour().integerValue
+        components.minute = LocalNotificationsManager.sharedManager.reminderTimeMinute().integerValue
+
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let date = calendar?.dateFromComponents(components)
+        
+        return NSDateFormatter.localizedStringFromDate(date!, dateStyle: NSDateFormatterStyle.NoStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+    }
+    
+    func on() -> Bool {
+        if NSUserDefaults.standardUserDefaults().objectForKey(notificationsOnKey) == nil {
+            return Bool(false)
+        }
+        return Bool(true)
+    }
+    
     func turnLocalNotificationsOff() {
+        NSUserDefaults.standardUserDefaults().removeObjectForKey(notificationsOnKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
         UIApplication.sharedApplication().cancelAllLocalNotifications()
     }
     
     func turnLocalNotificationsOn() {
+        NSUserDefaults.standardUserDefaults().setBool(true, forKey: notificationsOnKey)
+        NSUserDefaults.standardUserDefaults().synchronize()
         UIApplication.sharedApplication().cancelAllLocalNotifications()
+        UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound | UIUserNotificationType.Alert | UIUserNotificationType.Badge, categories: nil))
     }
     
-    func reminderTimeHour() -> NSNumber? {
-        return NSUserDefaults.standardUserDefaults().objectForKey(hourKey) as? NSNumber
-    }
-    
-    func setReminderTimeHour() {
+    func setReminder(hour: NSNumber, minute: NSNumber) {
         UIApplication.sharedApplication().cancelAllLocalNotifications()
+        var notification = UILocalNotification()
+        notification.alertBody = "Don't forget to enter your Beeminder data for today!"
+        notification.repeatInterval = NSCalendarUnit.DayCalendarUnit
+        
     }
     
-    func reminderTimeMinute() -> NSNumber? {
-        return NSUserDefaults.standardUserDefaults().objectForKey(minuteKey) as? NSNumber
+    func reminderTimeHour() -> NSNumber {
+        let storedHour: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey(hourKey)
+        if storedHour != nil {
+            return storedHour as! NSNumber
+        }
+        return self.defaultHour
+    }
+    
+    func reminderTimeMinute() -> NSNumber {
+        let storedMinute: AnyObject? = NSUserDefaults.standardUserDefaults().objectForKey(minuteKey)
+        if storedMinute != nil {
+            return storedMinute as! NSNumber
+        }
+        return self.defaultMinute
     }
 
 }
--- BeeSwift/SettingsViewController.swift
+++ BeeSwift/SettingsViewController.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
-    var dataEntryReminderLabel = BSLabel()
-    var dataEntryReminderSwitch = UISwitch()
+    private var dataEntryReminderLabel = BSLabel()
+    private var dataEntryReminderSwitch = UISwitch()
+    private var timePickerView = UIPickerView()
+    
+    private var animatingTimePickerView = false
+    private var timePickerViewVisible = false
 
     override func viewDidLoad() {
         self.title = "Settings"
@@ -27,10 +31,23 @@ class SettingsViewController: UIViewController {
         
         self.dataEntryReminderLabel.font = UIFont(name: "Avenir", size: 16)
         self.dataEntryReminderLabel.text = "Data entry reminder: off"
+        self.dataEntryReminderLabel.userInteractionEnabled = true
+        let tapGR = UITapGestureRecognizer()
+        tapGR.addTarget(self, action: "dataEntryReminderLabelTapped")
+        self.dataEntryReminderLabel.addGestureRecognizer(tapGR)
         self.view.addSubview(self.dataEntryReminderLabel)
         self.dataEntryReminderLabel.snp_makeConstraints { (make) -> Void in
             make.centerY.equalTo(self.dataEntryReminderSwitch)
             make.left.equalTo(self.dataEntryReminderSwitch.snp_right).offset(20)
+        }
+        
+        self.timePickerView.delegate = self
+        self.timePickerView.dataSource = self
+        self.view.addSubview(self.timePickerView)
+        self.timePickerView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.dataEntryReminderLabel.snp_bottom).offset(20)
+            make.width.equalTo(self.view)
+            make.height.equalTo(100)
         }
         
         var signOutButton = BSButton()
@@ -42,6 +59,35 @@ class SettingsViewController: UIViewController {
             make.bottom.equalTo(-30)
             make.width.equalTo(self.view).multipliedBy(0.75)
             make.centerX.equalTo(self.view)
+        }
+    }
+    
+    func dataEntryReminderLabelTapped() {
+        self.toggleTimePickerView()
+    }
+    
+    func toggleTimePickerView() {
+        if self.animatingTimePickerView {
+            return
+        }
+        self.animatingTimePickerView = true
+
+        UIView.animateWithDuration(0.25, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
+            self.timePickerView.snp_updateConstraints({ (make) -> Void in
+                make.height.equalTo(self.timePickerViewVisible ? 0 : 100)
+            })
+        }) { (flag) -> Void in
+            self.animatingTimePickerView = false
+            self.timePickerViewVisible = !self.timePickerViewVisible
+        }
+    }
+    
+    func updateDataEntryReminderLabel() {
+        if LocalNotificationsManager.sharedManager.on() {
+            self.dataEntryReminderLabel.text = "Data entry reminder: \(LocalNotificationsManager.sharedManager.humanizedReminderTime())"
+        }
+        else {
+            self.dataEntryReminderLabel.text = "Data entry reminder: off"
         }
     }
     
@@ -57,5 +103,68 @@ class SettingsViewController: UIViewController {
         else {
             LocalNotificationsManager.sharedManager.turnLocalNotificationsOff()
         }
+        self.updateDataEntryReminderLabel()
+    }
+    
+    func use24HourTime() -> Bool {
+        let formatString: NSString = NSDateFormatter.dateFormatFromTemplate("j", options: 0, locale: NSLocale.currentLocale())!
+        return !formatString.containsString("a")
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return self.use24HourTime() ? 2 : 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView {
+        let view = UIView()
+        let label = BSLabel()
+        view.addSubview(label)
+        label.snp_makeConstraints { (make) -> Void in
+            make.edges.equalTo(view)
+        }
+        label.font = UIFont(name: "Avenir", size: 17)
+        
+        var text = ""
+        var alignment = NSTextAlignment.Center
+        
+        if (component == 2) {
+            text = row == 0 ? "AM" : "PM"
+            alignment = NSTextAlignment.Left
+        }
+        else if (component == 1) {
+            text = row < 10 ? "0\(row)" : "\(row)"
+            alignment = NSTextAlignment.Center
+        }
+        else {
+            if (self.use24HourTime() || row <= 12) {
+                text = "\(row)"
+            }
+            else {
+                text = "\(row - 12)"
+            }
+            alignment = NSTextAlignment.Right
+        }
+        
+        label.text = text
+        label.textAlignment = alignment
+        
+        return view
+    }
+
+//    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+//        if component == 0 {
+//            return self.view.snp_width*0.4
+//        }
+//        return 100
+//    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return 24
+        }
+        else if component == 1 {
+            return 60
+        }
+        return 2
     }
 }
