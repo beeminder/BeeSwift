54e45a42f172c965567c3bb8677b7e7328c5a01b
Andy Brett <andy@andybrett.com>
2019-03-20 15:27:27 -0600

    dedupe stand hours and similar

--- BeeSwift/GoalExtension.swift
+++ BeeSwift/GoalExtension.swift
@@ -368,7 +368,21 @@ extension Goal {
             }
             return datapointValue
         }
+        
+        var uniqueSamples : [HKSample] = []
         samples.forEach { (sample) in
+            var dupe = false
+            uniqueSamples.forEach({ (seenSample) in
+                if seenSample.startDate == sample.startDate &&
+                     seenSample.endDate == sample.endDate &&
+                     seenSample.device  != sample.device {
+                    dupe = true
+                }
+            })
+            if !dupe { uniqueSamples.append(sample) }
+        }
+        
+        uniqueSamples.forEach { (sample) in
             datapointValue += self.hkDatapointValueForSample(sample: sample, units: units)
         }
         return datapointValue
