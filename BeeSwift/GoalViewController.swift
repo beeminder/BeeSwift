2d31b21a059b4bc8370f705b3331240d74a47fd5
Andy Brett <andy@andybrett.com>
2019-05-22 15:12:03 -0600

    better interface for sync spinner

--- BeeSwift/GoalViewController.swift
+++ BeeSwift/GoalViewController.swift
@@ -375,9 +375,11 @@ class GoalViewController: UIViewController, UITableViewDelegate, UITableViewData
         let hud = MBProgressHUD.showAdded(to: self.view, animated: true)
         hud?.mode = .indeterminate
         self.goal.hkQueryForLast(days: 1, success: {
-            DispatchQueue.main.async {
-                MBProgressHUD.hideAllHUDs(for: self.view, animated: true)
-            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: {
+                hud?.mode = .customView
+                hud?.customView = UIImageView(image: UIImage(named: "checkmark"))
+                hud?.hide(true, afterDelay: 2)
+            })
         }) {
             DispatchQueue.main.async {
                 MBProgressHUD.hideAllHUDs(for: self.view, animated: true)
