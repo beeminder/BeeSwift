9aea8516446289138c7c143e5cd674c61c038597
krugerk <4656811+krugerk@users.noreply.github.com>
2020-08-26 12:29:40 +0200

    Configure for re-use for another cell
    the new goal cell presents just one button which
    is not configurable

--- BeeSwift/NewGoalCollectionViewCell.swift
+++ BeeSwift/NewGoalCollectionViewCell.swift
@@ -9,28 +9,28 @@
 import UIKit
 
 class NewGoalCollectionViewCell: UICollectionViewCell {
-    
-    var newGoalButton = BSButton()
-    
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.contentView.addSubview(self.newGoalButton)
-        self.newGoalButton.addTarget(self, action: #selector(NewGoalCollectionViewCell.newGoalButtonPressed), for: .touchUpInside)
-        self.newGoalButton.snp.makeConstraints { (make) in
+
+        let newGoalButton = BSButton()
+        self.contentView.addSubview(newGoalButton)
+        newGoalButton.addTarget(self, action: #selector(NewGoalCollectionViewCell.newGoalButtonPressed), for: .touchUpInside)
+        newGoalButton.snp.makeConstraints { (make) in
             make.top.equalTo(self.contentView).offset(25)
             make.width.equalTo(self.contentView).multipliedBy(0.75)
             make.centerX.equalTo(self.contentView)
             make.bottom.equalTo(self.contentView).offset(-25)
         }
-        self.newGoalButton.setTitle("Create Goal", for: .normal)
+        newGoalButton.setTitle("Create Goal", for: .normal)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     @objc func newGoalButtonPressed() {
         NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: "createGoalButtonPressed")))
     }
-    
+
 }
