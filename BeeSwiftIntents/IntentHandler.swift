586852a140e61da24620dd4d8dcb98443cf95130
Theo Spears <git@theospears.com>
2022-09-05 12:22:18 -0400

    Address review feedback

--- BeeSwiftIntents/IntentHandler.swift
+++ BeeSwiftIntents/IntentHandler.swift
@@ -3,7 +3,6 @@
 //  BeeSwiftIntents
 //
 //  Created by Theo Spears on 9/1/22.
-//  Copyright © 2022 APB. All rights reserved.
 //
 
 import Foundation
@@ -12,6 +11,6 @@ import Intents
 @available(iOS 14.0, *)
 class IntentHandler : INExtension {
     override func handler(for intent: INIntent) -> Any? {
-        return AddDataIntentHandler()
+        AddDataIntentHandler()
     }
 }
