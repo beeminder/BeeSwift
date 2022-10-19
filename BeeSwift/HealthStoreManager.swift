09e8efe2af0c251902957488a36a9b48983c7d53
Theo Spears <github@theospears.com>
2022-10-18 20:45:25 -0700

    Isolate HealthKit interactions from the rest of the codebase (#325)
    This PR takes two steps towards isolating HealthKit logic from the rest of the codebase
    1. It removes logic from the JSONGoal class, and instead moves it to a HealthKitConnection class
    2. It moves all health kit data model operations to behind HealthStoreManager
    
    This reduction in surface area should make it much simpler to refactor the health kit code.
    
    Test Plan:
    * Run on device and observe sync with health kit buttons work
    * Also add significant logging and observe it is run as expected
    
    Note that this code cannot be correctly tested on simulator as it does not have data available.

--- BeeSwift/HealthStoreManager.swift
+++ BeeSwift/HealthStoreManager.swift
@@ -10,11 +10,66 @@ import Foundation
 import HealthKit
 
 class HealthStoreManager :NSObject {
-    
     static let sharedManager = HealthStoreManager()
+
     var healthStore : HKHealthStore?
+
+    /// The Connection objects responsible for updating goals based on their healthkit metrics
+    /// Dictionary key is the goal id, as this is stable across goal renames
+    private var connections: [String: GoalHealthKitConnection] = [:]
     
-    func setupHealthkit() {
+    private func ensureHealthStoreCreated() {
         self.healthStore = HKHealthStore()
+    }
+
+    /// Gets or creates an appropriate connection object for the supplied goal
+    private func connectionFor(goal: JSONGoal) -> GoalHealthKitConnection? {
+        if goal.healthKitMetric == "" {
+            // Goal does not have a metric. Make sure any connection is removed
+            connections.removeValue(forKey: goal.id)
+            return nil
+        } else {
+            return connections[goal.id] ?? GoalHealthKitConnection(goal: goal)
+        }
+    }
+
+    func requestAuthorization(goals: [JSONGoal], completion: @escaping (Bool, Error?) -> Void) {
+        ensureHealthStoreCreated();
+        let goalConnections = goals.map { self.connectionFor(goal:$0) }.compactMap { $0 }
+
+        var permissions = Set<HKObjectType>.init()
+        goalConnections.forEach { (connection) in
+            if let permissionType = connection.hkPermissionType() {
+               permissions.insert(permissionType)
+            }
+        }
+        guard permissions.count > 0 else { return }
+
+        guard let healthStore = HealthStoreManager.sharedManager.healthStore else { return }
+        healthStore.requestAuthorization(toShare: nil, read: permissions, completion: completion)
+    }
+
+    func setupHealthKitGoals(goals: [JSONGoal]) {
+        ensureHealthStoreCreated();
+        let goalConnections = goals.map { self.connectionFor(goal:$0) }.compactMap { $0 }
+        goalConnections.forEach { (connection) in
+            connection.setupHealthKit()
+        }
+    }
+
+    func setupHealthKitGoal(goal: JSONGoal) {
+        ensureHealthStoreCreated();
+        if let connection = self.connectionFor(goal: goal) {
+            connection.setupHealthKit()
+        }
+    }
+
+    func syncHealthKitData(goal: JSONGoal, days: Int, success: (() -> ())?, errorCompletion: (() -> ())?) {
+        ensureHealthStoreCreated();
+        if let connection = self.connectionFor(goal: goal) {
+            connection.hkQueryForLast(days: days, success: success, errorCompletion: errorCompletion)
+        } else {
+            errorCompletion?()
+        }
     }
 }
