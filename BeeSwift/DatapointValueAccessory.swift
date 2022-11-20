01597e6e2a0daf6d26b4303e4749b9d54f9d78d0
Theo Spears <github@theospears.com>
2022-11-19 21:58:30 -0500

    Update colon keyboard accessory to match keyboard look
    This restyles the ":" button added above the keyboard to much more closely
    resemble other keyboard buttons. It is based on the appearance of iOS 16 so
    may look less similar on other iOS versions. Unfortunately there are not
    common controls provided for this, so we match by carefully looking at the
    pixels.
    
    Test Plan:
    * Check the look in light and dark mode
    * Check transitioning between the two modes works in simulator

--- BeeSwift/DatapointValueAccessory.swift
+++ BeeSwift/DatapointValueAccessory.swift
@@ -9,11 +9,12 @@
 import Foundation
 import UIKit
 
-class DatapointValueAccessory : UIView {
+class DatapointValueAccessory : UIInputView {
     var valueField : UITextField?
+    var colonButton: UIButton?
 
     init() {
-        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 44))
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 44), inputViewStyle: .keyboard)
         setupView()
     }
 
@@ -23,27 +24,53 @@ class DatapointValueAccessory : UIView {
     }
 
     private func setupView() {
-        self.backgroundColor = UIColor.white
-
         let colonButton = UIButton()
+        self.colonButton = colonButton
         self.addSubview(colonButton)
         self.clipsToBounds = true
+
         colonButton.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(self).multipliedBy(1.0/3.0).offset(-1)
-            make.height.equalTo(self)
-            make.left.equalTo(-1)
-            make.top.equalTo(0)
+            make.width.equalTo(self).multipliedBy(1.0/3.0).offset(-8)
+            make.bottom.equalTo(-1)
+            make.left.equalTo(7)
+            make.top.equalTo(4)
         }
         colonButton.setTitle(":", for: UIControl.State())
-        colonButton.layer.borderWidth = 1
-        colonButton.layer.borderColor = UIColor.beeminder.gray.cgColor
-        colonButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 26)
-        colonButton.setTitleColor(UIColor.black, for: UIControl.State())
         colonButton.addTarget(self, action: #selector(self.colonButtonPressed), for: .touchUpInside)
+
+        // Style component to look similar to other keyboard buttons
+        colonButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 26)
+        colonButton.layer.cornerRadius = 5
+        colonButton.layer.shadowColor = UIColor.black.cgColor
+        colonButton.layer.shadowOffset = CGSize(width: 0, height: 0.5)
+        colonButton.layer.shadowRadius = 0
+        colonButton.layer.shadowOpacity = 0.5
+
+        updateColorsToMatchMode()
     }
+
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateColorsToMatchMode()
+    }
+
+    private func updateColorsToMatchMode() {
+        guard let colonButton = self.colonButton else { return }
+
+        if traitCollection.userInterfaceStyle == .dark {
+            // Chosen to match keyboard button color in dark mode
+            colonButton.backgroundColor = UIColor(white: 0.42, alpha: 1.0)
+            colonButton.setTitleColor(.white, for: .normal)
+        } else {
+            colonButton.backgroundColor = .white
+            colonButton.setTitleColor(.black, for: .normal)
+        }
+    }
+
 
     @objc func colonButtonPressed() {
         guard let valueField = self.valueField else { return }
         valueField.text = "\(valueField.text!):"
     }
+
 }
