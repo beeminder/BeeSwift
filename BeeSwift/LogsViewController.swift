d0fe8876eca4e72dd28c8f2388a60308a183f9d3
Theo Spears <github@theospears.com>
2023-04-21 22:37:21 -0700

    Add a screen to view and save logs
    When debugging issues with background notifications it has been a pain
    to use sysdiagnose to extract and find the logs. To simplify the process
    the app can now display its own logs. Also make it possible to share the
    logs via normal mechanisms, for example to share with support.
    
    Testing:
    Manually tested viewing and sharing logs
    Added unit tests for the log reading/saving code

---
+++ BeeSwift/LogsViewController.swift
@@ -0,0 +1,122 @@
+//  LogsViewController.swift
+//  BeeSwift
+//
+//  A screen to view and share application logs
+
+import Foundation
+import OSLog
+import OrderedCollections
+
+
+@available(iOS 15.0, *)
+class LogsViewController: UIViewController {
+
+    let logTextView = UITextView()
+    let systemLogsToggle = UIButton()
+    let errorLevelButton = UIButton()
+
+    let logReader = LogReader()
+
+    var showSystemMessages = false
+    var errorLevel = OSLogEntryLog.Level.debug
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .black
+        self.title = "Logs"
+
+        // Add a share link to the navigationcontroller which shares the contents of the log when clicked
+        let shareButton = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareLogs))
+        self.navigationItem.rightBarButtonItem = shareButton
+
+        view.addSubview(systemLogsToggle)
+        systemLogsToggle.setTitle("App Only", for: .normal)
+        systemLogsToggle.configuration = .gray()
+        systemLogsToggle.snp.makeConstraints{ (make) in
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.leftMargin).offset(10)
+            make.top.equalTo(view)
+        }
+        systemLogsToggle.addTarget(self, action: #selector(systemMessagesToggleTapped), for: .touchUpInside)
+
+        view.addSubview(errorLevelButton)
+        errorLevelButton.setTitle("Debug", for: .normal)
+        errorLevelButton.configuration = .gray()
+        errorLevelButton.snp.makeConstraints{ (make) in
+            make.left.equalTo(systemLogsToggle.snp.right).offset(10)
+            make.right.equalTo(view.safeAreaLayoutGuide.snp.rightMargin).offset(-10)
+            make.top.equalTo(view)
+            make.bottom.equalTo(systemLogsToggle.snp.bottom)
+            make.width.equalTo(systemLogsToggle.snp.width)
+        }
+        errorLevelButton.addTarget(self, action: #selector(errorLevelButtonTapped), for: .touchUpInside)
+
+        view.addSubview(logTextView)
+        logTextView.isEditable = false
+        logTextView.isScrollEnabled = true
+        logTextView.snp.makeConstraints { (make) in
+            make.top.equalTo(systemLogsToggle.snp.bottom).offset(10)
+            make.left.right.bottom.equalTo(view)
+        }
+
+        loadLogs()
+    }
+
+    func loadLogs() {
+        Task { @MainActor in
+            let hud = MBProgressHUD.showAdded(to: view, animated: false)
+            self.logTextView.text = await self.logReader.getLogMessages(showSystemMessages: showSystemMessages, errorLevel: errorLevel)
+            hud.hide(animated: false)
+        }
+    }
+
+    @objc
+    func shareLogs() {
+        Task { @MainActor in
+            let logFile = await logReader.saveLogsToFile(showSystemMessages: showSystemMessages, errorLevel: errorLevel)
+            let activityViewController = UIActivityViewController(activityItems: [logFile], applicationActivities: nil)
+            activityViewController.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+            activityViewController.completionWithItemsHandler = { activityType, completed, returnedItems, error in
+                try? FileManager.default.removeItem(at: logFile)
+            }
+            self.present(activityViewController, animated: true, completion: nil)
+        }
+    }
+
+    @objc
+    func systemMessagesToggleTapped() {
+        showSystemMessages = !showSystemMessages
+        if showSystemMessages {
+            systemLogsToggle.setTitle("App and System", for: .normal)
+        } else {
+            systemLogsToggle.setTitle("App Only", for: .normal)
+        }
+        loadLogs()
+    }
+
+    @objc
+    func errorLevelButtonTapped() {
+        let errorLevelAlert = UIAlertController(title: "Choose Error Level", message: nil, preferredStyle: .actionSheet)
+
+        let errorLevels: OrderedDictionary<String, OSLogEntryLog.Level> = [
+            "Debug": .debug,
+            "Info": .info,
+            "Notice": .notice,
+            "Error": .error,
+            "Fault": .fault
+        ]
+
+        for (title, level) in errorLevels {
+            let action = UIAlertAction(title: title, style: .default) { _ in
+                self.errorLevel = level
+                self.errorLevelButton.setTitle(title, for: .normal)
+                self.loadLogs()
+            }
+            errorLevelAlert.addAction(action)
+        }
+        errorLevelAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(errorLevelAlert, animated: true, completion: nil)
+    }
+}
+
