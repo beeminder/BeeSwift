756ab96aa626a7c6dabd428e56993b7df6ebf921
Theo Spears <github@theospears.com>
2023-06-25 13:41:15 -0700

    Address warnings from current XCode version

--- BeeSwift/GoalManager.swift
+++ BeeSwift/GoalManager.swift
@@ -212,7 +212,7 @@ actor GoalManager {
         let todayGoals = goals.values.map { (goal) in
             let shortSlug = goal.slug.prefix(20)
             let limsum = goal.limsum ?? ""
-            return ["deadline" : goal.deadline.intValue, "thumbUrl": goal.cacheBustingThumbUrl, "limSum": "\(shortSlug): \(limsum)", "slug": goal.slug, "hideDataEntry": goal.hideDataEntry()]
+            return ["deadline" : goal.deadline.intValue, "thumbUrl": goal.cacheBustingThumbUrl, "limSum": "\(shortSlug): \(limsum)", "slug": goal.slug, "hideDataEntry": goal.hideDataEntry()] as [String : Any]
         }
         return Array(todayGoals.prefix(3)) as Array<Any>
     }
