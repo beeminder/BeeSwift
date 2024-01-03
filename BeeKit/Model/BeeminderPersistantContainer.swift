e869dd04fe0ec9e59f1505b80d20f944c8e63b71
Theo Spears <github@theospears.com>
2024-01-02 21:51:04 -0800

    Remove needless subdirectory

--- BeeKit/Model/BeeminderPersistantContainer.swift
+++ BeeKit/Model/BeeminderPersistantContainer.swift
@@ -5,7 +5,6 @@ class BeeminderPersistentContainer: NSPersistentContainer {
 
     override open class func defaultDirectoryURL() -> URL {
         var storeURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.beeminder.beeminder")
-        storeURL = storeURL?.appendingPathComponent("beeminder.sqlite")
         return storeURL!
     }
 
