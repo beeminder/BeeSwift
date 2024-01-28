684e79582202094d315cb318203b9ed55681b373
Theo Spears <github@theospears.com>
2024-01-27 20:29:14 -0800

    Use daystamp for things

--- BeeKit/HeathKit/QuantityHealthKitMetric.swift
+++ BeeKit/HeathKit/QuantityHealthKitMetric.swift
@@ -35,14 +35,18 @@ class QuantityHealthKitMetric : HealthKitMetric {
         guard let quantityType = HKObjectType.quantityType(forIdentifier: self.hkQuantityTypeIdentifier) else {
             throw HealthKitError("Unable to look up a quantityType")
         }
-        let predicate = self.predicateForLast(days: days, deadline: deadline)
+
+        let today = Daystamp.now(deadline: deadline)
+        let startDate = today - days
+        let predicate = HKQuery.predicateForSamples(withStart: startDate.start(deadline: deadline), end: nil)
+
         let options : HKStatisticsOptions = quantityType.aggregationStyle == .cumulative ? .cumulativeSum : .discreteMin
 
         let statsCollection = try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<HKStatisticsCollection, Error>) in
             let query = HKStatisticsCollectionQuery(quantityType: quantityType,
                                                     quantitySamplePredicate: predicate,
                                                     options: options,
-                                                    anchorDate: anchorDate(deadline: deadline),
+                                                    anchorDate: today.start(deadline: deadline),
                                                     intervalComponents: DateComponents(day:1))
             query.initialResultsHandler = {
                 query, collection, error in
@@ -58,7 +62,7 @@ class QuantityHealthKitMetric : HealthKitMetric {
         }
 
         // TODO: It would be possible for Apple to give us more data than requested in the statsCollection, we should consider passing through the number of days to filter here
-        return try await datapointsForCollection(collection: statsCollection, days: days, deadline: deadline, healthStore: healthStore)
+        return try await datapointsForCollection(collection: statsCollection, startDate: startDate, endDate: today, deadline: deadline, healthStore: healthStore)
     }
 
     func units(healthStore : HKHealthStore) async throws -> HKUnit {
@@ -69,53 +73,16 @@ class QuantityHealthKitMetric : HealthKitMetric {
         }
         return unit
     }
-
-    private func predicateForLast(days : Int, deadline : Int) -> NSPredicate? {
-        let startTime = goalAwareStartOfDay(days: days, deadline: deadline)
-        return HKQuery.predicateForSamples(withStart: startTime, end: nil)
-    }
-
-    private func goalAwareStartOfDay(days : Int, deadline : Int) -> Date {
-        let calendar = Calendar.current
-
-        let components = calendar.dateComponents(in: TimeZone.current, from: Date())
-        let localMidnightThisMorning = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: calendar.date(from: components)!)
-
-        let startOfToday = calendar.date(byAdding: .second, value: deadline, to: localMidnightThisMorning!)
-
-        let dayOffset = -days + 1 // One day should fetch only today
-        return calendar.date(byAdding: .day, value: dayOffset, to: startOfToday!)!
-    }
-
-    private func anchorDate(deadline : Int) -> Date {
-        // TODO: This will use the device local timezone instead of the user's beeminder timezone
-        let calendar = Calendar.current
-        var anchorComponents = calendar.dateComponents([.day, .month, .year, .weekday], from: Date())
-
-        anchorComponents.hour = 0
-        anchorComponents.minute = 0
-        anchorComponents.second = 0
-
-        guard let midnight = calendar.date(from: anchorComponents) else {
-            fatalError("*** unable to create a valid date from the given components ***")
-        }
-        return calendar.date(byAdding: .second, value: deadline, to: midnight)!
-    }
-
-    private func datapointsForCollection(collection : HKStatisticsCollection, days: Int, deadline : Int, healthStore: HKHealthStore) async throws -> [DataPoint] {
-        let endDate = Date()
-        let calendar = Calendar.current
-        guard let startDate = calendar.date(byAdding: .day, value: -days, to: endDate) else {
-            throw HealthKitError("Cannot find calculate start date")
-        }
+    
+    private func datapointsForCollection(collection : HKStatisticsCollection, startDate: Daystamp, endDate: Daystamp, deadline : Int, healthStore: HKHealthStore) async throws -> [DataPoint] {
 
         var results : [DataPoint] = []
 
         for statistics in collection.statistics() {
-            let datapointDate = deadline >= 0 ? statistics.startDate : statistics.endDate
+            let daystamp = Daystamp(fromDate: statistics.startDate, deadline: deadline)
 
             // Ignore statistics outside our window
-            if datapointDate < startDate || datapointDate > endDate {
+            if daystamp < startDate || daystamp > endDate {
                 continue
             }
 
@@ -144,12 +111,8 @@ class QuantityHealthKitMetric : HealthKitMetric {
                 continue
             }
 
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyyMMdd"
-            let daystamp = formatter.string(from: datapointDate)
-
-            let id = "apple-health-" + daystamp
-            results.append(NewDataPoint(requestid: id, daystamp: try Daystamp(fromString: daystamp), value: NSNumber(value: datapointValue), comment: "Auto-entered via Apple Health"))
+            let id = "apple-health-" + daystamp.description
+            results.append(NewDataPoint(requestid: id, daystamp: daystamp, value: NSNumber(value: datapointValue), comment: "Auto-entered via Apple Health"))
         }
 
         return results
