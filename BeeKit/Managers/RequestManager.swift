9fad795404106fc5ae4a9ae28d4a4130ea6b8056
Theo Spears <github@theospears.com>
2024-02-22 16:27:19 -0800

    Use headers to authenticate against the beeminder API

--- BeeKit/Managers/RequestManager.swift
+++ BeeKit/Managers/RequestManager.swift
@@ -29,9 +29,15 @@ public class RequestManager {
     public let baseURLString = Config.init().baseURLString
     private let logger = Logger(subsystem: "com.beeminder.beeminder", category: "RequestManager")
     
-    func rawRequest(url: String, method: HTTPMethod, parameters: [String: Any]?) async throws -> Any? {
+    func rawRequest(url: String, method: HTTPMethod, parameters: [String: Any]?, headers: HTTPHeaders) async throws -> Any? {
         let encoding: ParameterEncoding = if method == .get { URLEncoding.default } else { JSONEncoding.default }
-        let response = await AF.request("\(baseURLString)/\(url)", method: method, parameters: parameters, encoding: encoding, headers: HTTPHeaders.default)
+        let response = await AF.request(
+            "\(baseURLString)/\(url)",
+            method: method,
+            parameters: parameters,
+            encoding: encoding,
+            headers: HTTPHeaders.default + headers
+        )
             .validate()
             .serializingData(emptyRequestMethods: [HTTPMethod.post])
             .response
@@ -64,36 +70,44 @@ public class RequestManager {
     }
     
     public func get(url: String, parameters: [String: Any]?) async throws -> Any? {
-        return try await rawRequest(url: url, method: .get, parameters: authedParams(parameters))
+        return try await rawRequest(url: url, method: .get, parameters: parameters, headers: authenticationHeaders())
     }
     
     
     public func put(url: String, parameters: [String: Any]?) async throws -> Any? {
-        return try await rawRequest(url: url, method: .patch, parameters: authedParams(parameters))
+        return try await rawRequest(url: url, method: .patch, parameters: parameters, headers: authenticationHeaders())
     }
     
     public func post(url: String, parameters: [String: Any]?) async throws -> Any? {
-        return try await rawRequest(url: url, method: .post, parameters: authedParams(parameters))
+        return try await rawRequest(url: url, method: .post, parameters: parameters, headers: authenticationHeaders())
     }
     
     public func delete(url: String, parameters: [String: Any]?) async throws -> Any? {
-        return try await rawRequest(url: url, method: .delete, parameters: authedParams(parameters))
+        return try await rawRequest(url: url, method: .delete, parameters: parameters, headers: authenticationHeaders())
     }
     
     
-    func authedParams(_ params: [String: Any]?) -> Parameters? {
-        if params == nil { return ["access_token" : ServiceLocator.currentUserManager.accessToken ?? ""] }
-        if ServiceLocator.currentUserManager.accessToken != nil {
-            var localParams = params!
-            localParams["access_token"] = ServiceLocator.currentUserManager.accessToken!
-            return localParams
+    func authenticationHeaders() -> HTTPHeaders {
+        guard let accessToken = ServiceLocator.currentUserManager.accessToken else {
+            return HTTPHeaders()
         }
-        return params
+        return HTTPHeaders([
+            HTTPHeader(name: "Authorization", value: "Bearer " + accessToken)
+        ])
     }
 
     public func addDatapoint(urtext: String, slug: String) async throws -> Any? {
         let params = ["urtext": urtext, "requestid": UUID().uuidString]
         
         return try await post(url: "api/v1/users/\(ServiceLocator.currentUserManager.username!)/goals/\(slug)/datapoints.json", parameters: params)
+    }
+}
+
+extension HTTPHeaders {
+    static func + (lhs: HTTPHeaders, rhs: HTTPHeaders) -> HTTPHeaders {
+        var allHeaders = [HTTPHeader]()
+        allHeaders.append(contentsOf: lhs)
+        allHeaders.append(contentsOf: rhs)
+        return HTTPHeaders(allHeaders)
     }
 }
