77975017b189775f6056f4805afb7dc31faa685b
Theo Spears <github@theospears.com>
2024-05-05 21:09:28 -0700

    Add types needed by today

--- BeeKit/Model/Goal.swift
+++ BeeKit/Model/Goal.swift
@@ -9,6 +9,12 @@ public class Goal: NSManagedObject {
     @NSManaged public var id: String
     @NSManaged public var slug: String
 
+    @NSManaged public var autodata: String?
+    @NSManaged public var deadline: Int
+    @NSManaged public var limSum: String
+    @NSManaged public var thumbUrl: String
+    @NSManaged public var won: Bool
+
     public init(context: NSManagedObjectContext, owner: User, id: String, slug: String) {
         let entity = NSEntityDescription.entity(forEntityName: "Goal", in: context)!
         super.init(entity: entity, insertInto: context)
@@ -44,5 +50,17 @@ public class Goal: NSManagedObject {
     // Question: Should this type know about JSON, or should there be an adapter / extension?
     public func updateToMatch(json: JSON) {
         self.slug = json["slug"].stringValue
+    }
+
+    public var isDataProvidedAutomatically: Bool {
+        guard let autodata = self.autodata else {
+            return false
+        }
+        return !autodata.isEmpty
+    }
+
+    public var hideDataEntry: Bool {
+        // TODO: Implement this
+        return isDataProvidedAutomatically || won
     }
 }
