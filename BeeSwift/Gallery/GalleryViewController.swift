86ac780789227243c74f0bf1166941c30434bad9
krugerk <4656811+krugerk@users.noreply.github.com>
2024-11-12 14:35:33 +0100

    clean code

--- BeeSwift/Gallery/GalleryViewController.swift
+++ BeeSwift/Gallery/GalleryViewController.swift
@@ -381,8 +381,7 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
         self.updateDeadbeatVisibility()
         self.lastUpdated = Date()
         self.updateLastUpdatedLabel()
-        let searchItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(self.searchButtonPressed))
-        self.navigationItem.leftBarButtonItem = searchItem
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(self.searchButtonPressed))
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
