515aeded18087921cf706e434adc39019a78dcba
krugerk <4656811+krugerk@users.noreply.github.com>
2024-11-30 11:16:16 +0100

    remove cruft

--- BeeKit/UI/UIColorExtension.swift
+++ BeeKit/UI/UIColorExtension.swift
@@ -12,16 +12,9 @@ import UIKit
 extension UIColor {
     
     public struct Beeminder {
-        public static let green = UIColor(red: 81.0/255.0,
-                                          green: 163.0/255.0,
-                                          blue: 81.0/255.0,
-                                          alpha: 1)
-        
-        public static let blue: UIColor = .systemBlue
-        public static let orange: UIColor = .systemOrange
         public static let red: UIColor = .systemRed
         
-        public static let gray = UIColor(white: 0.7, alpha: 1.0)
+        public static let gray = UIColor.systemGray
         
         public static let yellow = UIColor(red: 255.0/255.0,
                                            green: 217.0/255.0,
