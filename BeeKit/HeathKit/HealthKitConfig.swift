e67b50b0a2bfe867b8818a32d61bb3ae3f20fe4a
krugerk <4656811+krugerk@users.noreply.github.com>
2024-12-17 00:28:43 +0100

    toothbrushing as seconds per day
    MVP of #88

--- BeeKit/HeathKit/HealthKitConfig.swift
+++ BeeKit/HeathKit/HealthKitConfig.swift
@@ -51,6 +51,9 @@ public enum HealthKitConfig {
             QuantityHealthKitMetric(humanText: "Vitamin K", databaseString: "dietaryVitaminK", category: .Nutrition, hkQuantityTypeIdentifier: .dietaryVitaminK),
             QuantityHealthKitMetric(humanText: "Water", databaseString: "water", category: .Nutrition, hkQuantityTypeIdentifier: .dietaryWater),
             
+            // Self care
+            ToothbrushingHealthKitMetric.make(),
+            
             // Sleep
             TimeInBedHealthKitMetric(humanText: "Time in bed", databaseString: "timeInBed", category: .Sleep),
             TimeAsleepHealthKitMetric(humanText: "Time asleep", databaseString: "timeAsleep", category: .Sleep),
