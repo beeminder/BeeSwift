24125bdf800b2485664a3e4a34207173fa2f896a
Theo Spears <github@theospears.com>
2024-12-16 14:17:48 -0800

    Move from StoryBoard to SceneDelegate
    This removes the main storyboard, which was the main piece of UI specified via config rather than code. Instead we use SceneDelegate. This would also allow more fine-grained control of multi-window configuration in the future if we want it.

---
+++ BeeSwift/SceneDelegate.swift
@@ -0,0 +1,16 @@
+// Part of BeeSwift. Copyright Beeminder
+
+import Foundation
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: GalleryViewController())
+        window?.makeKeyAndVisible()
+    }
+}
