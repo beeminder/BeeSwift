057455806586f42680ec416210fba4885e1a9d40
krugerk <4656811+krugerk@users.noreply.github.com>
2025-01-07 09:49:09 +0100

    bumped to ios13+ collectionview diffabledatasource and snapshot
    These are more modern approaches to managing a collectionView's
    contents.
    As before, the collection shows a single section and merely displays
    all of the filtered goals in order.

--- BeeSwift/Gallery/GalleryViewController.swift
+++ BeeSwift/Gallery/GalleryViewController.swift
@@ -18,7 +18,7 @@ import CoreData
 import BeeKit
 
 
-class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate, SFSafariViewControllerDelegate {
+class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UISearchBarDelegate, SFSafariViewControllerDelegate {
     let logger = Logger(subsystem: "com.beeminder.beeminder", category: "GalleryViewController")
 
     // Dependencies
@@ -43,6 +43,8 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
     
     var goals : [Goal] = []
     var filteredGoals : [Goal] = []
+    
+    var dataSource: UICollectionViewDiffableDataSource<Int, Goal>!
     
     public enum NotificationName {
         public static let openGoal = Notification.Name(rawValue: "com.beeminder.openGoal")
@@ -84,11 +86,25 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
             make.bottom.equalTo(stackView.keyboardLayoutGuide.snp.top)
         }
 
+        
+        
         self.collectionViewLayout = UICollectionViewFlowLayout()
         self.collectionView = UICollectionView(frame: stackView.frame, collectionViewLayout: self.collectionViewLayout!)
         self.collectionView?.backgroundColor = .systemBackground
         self.collectionView?.alwaysBounceVertical = true
         self.collectionView?.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "footer")
+        
+        self.dataSource = .init(collectionView: collectionView!, cellProvider: { collectionView, indexPath, itemIdentifier in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: self.cellReuseIdentifier, for: indexPath) as! GoalCollectionViewCell
+            cell.goal = self.filteredGoals[indexPath.row]
+            
+            return cell
+        })
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            let footerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "footer", for: indexPath)
+            return footerView
+        }
+        self.collectionView?.dataSource = dataSource
 
         self.view.backgroundColor = .systemBackground
         self.title = "Goals"
@@ -149,7 +165,7 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
 
         self.collectionContainer.addSubview(self.collectionView!)
         self.collectionView!.delegate = self
-        self.collectionView!.dataSource = self
+        
         self.collectionView!.register(GoalCollectionViewCell.self, forCellWithReuseIdentifier: self.cellReuseIdentifier)
         self.collectionView?.snp.makeConstraints { (make) in
             make.top.bottom.equalTo(collectionContainer)
@@ -217,6 +233,11 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
             make.right.equalTo(self.view.safeAreaLayoutGuide.snp.rightMargin)
             make.bottom.equalTo(self.collectionView!.keyboardLayoutGuide.snp.top)
         }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Goal>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(filteredGoals)
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -269,7 +290,12 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
     @objc func handleSignOut() {
         self.goals = []
         self.filteredGoals = []
-        self.collectionView?.reloadData()
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Goal>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(filteredGoals)
+        dataSource.apply(snapshot, animatingDifferences: true)
+        
         if self.presentedViewController != nil {
             if type(of: self.presentedViewController!) == SignInViewController.self { return }
         }
@@ -376,7 +402,12 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
         self.setupHealthKit()
         self.collectionView?.refreshControl?.endRefreshing()
         MBProgressHUD.hide(for: self.view, animated: true)
-        self.collectionView!.reloadData()
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Goal>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(filteredGoals)
+        dataSource.apply(snapshot, animatingDifferences: true)
+        
         self.updateDeadbeatVisibility()
         self.lastUpdated = Date()
         self.updateLastUpdatedLabel()
@@ -393,10 +424,6 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return versionManager.lastChckedUpdateState() == .UpdateRequired ? 0 : self.filteredGoals.count
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -423,16 +450,6 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
         return CGSize(width: targetWidth, height: 120)
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell:GoalCollectionViewCell = self.collectionView!.dequeueReusableCell(withReuseIdentifier: self.cellReuseIdentifier, for: indexPath) as! GoalCollectionViewCell
-        
-        let goal:Goal = self.filteredGoals[(indexPath as NSIndexPath).row]
-
-        cell.goal = goal
-        
-        return cell
-    }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
         return CGSize(width: 320, height: section == 0 && self.goals.count > 0 ? 5 : 0)
     }
@@ -445,8 +462,12 @@ class GalleryViewController: UIViewController, UICollectionViewDelegateFlowLayou
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         // After a rotation or other size change the optimal width for our cells may have changed.
         // We instruct the collectionView to reload so widths are recalculated.
-        coordinator.animate { _ in } completion: { _ in
-            self.collectionView?.reloadData()
+        coordinator.animate { _ in } completion: { [weak self] _ in
+            guard let self else { return }
+            var snapshot = NSDiffableDataSourceSnapshot<Int, Goal>()
+            snapshot.appendSections([0])
+            snapshot.appendItems(filteredGoals)
+            dataSource.apply(snapshot, animatingDifferences: true)
         }
     }
 
