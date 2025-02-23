beeminder/BeeSwift

commit 758d7feb957130f789c5953168f0077d59eb6f91

Move from StoryBoard to SceneDelegate (#555)
## Summary
Migrate from using a StoryBoard to using SceneDelegate for the main UI
definition.

While almost none of the app was defined via StoryBoard, the top level
still was. This made it harder to open PRs e.g. to change the root view.
As we weren't using the storyboard for anything, instead migrate to
using SceneDelegate to manage this.

## Validation
* [X] Validate the app launches in simulator and shows gallery
* [x] Validate background, relaunch, etc works as before
* [x] Check on iPad the split screen / window behavior has not changed

BeeSwift/AppDelegate.swift
@@ -21,8 +21,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     let logger = Logger(subsystem: "com.beeminder.beeminder", category: "AppDelegate")
     let backgroundUpdates = BackgroundUpdates()
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         logger.notice("application:didFinishLaunchingWithOptions")
 

BeeSwift/SceneDelegate.swift
@@ -1,0 +1,17 @@
+// Part of BeeSwift. Copyright Beeminder
+
+import Foundation
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .all))
+
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: GalleryViewController())
+        window?.makeKeyAndVisible()
+    }
+}

BeeSwift.xcodeproj/project.pbxproj
@@ -24,7 +24,6 @@
 		A17E930C1B09032F0098FCA0 /* QuartzCore.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = A17E930B1B09032F0098FCA0 /* QuartzCore.framework */; };
 		A196CB1A1AE4142E00B90A3E /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = A196CB191AE4142E00B90A3E /* AppDelegate.swift */; };
 		A196CB1F1AE4142F00B90A3E /* GalleryViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = A196CB1E1AE4142F00B90A3E /* GalleryViewController.swift */; };
-		A196CB221AE4142F00B90A3E /* Main.storyboard in Resources */ = {isa = PBXBuildFile; fileRef = A196CB201AE4142F00B90A3E /* Main.storyboard */; };
 		A196CB241AE4142F00B90A3E /* Images.xcassets in Resources */ = {isa = PBXBuildFile; fileRef = A196CB231AE4142F00B90A3E /* Images.xcassets */; };
 		A196CB331AE4142F00B90A3E /* BeeSwiftTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = A196CB321AE4142F00B90A3E /* BeeSwiftTests.swift */; };
 		A1A8BDE61FEAE8DD007D61D6 /* ConfigureNotificationsViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = A1A8BDE51FEAE8DD007D61D6 /* ConfigureNotificationsViewController.swift */; };
@@ -38,6 +37,7 @@
 		A1E618E41E7934C700D8ED93 /* HealthKitConfigTableViewCell.swift in Sources */ = {isa = PBXBuildFile; fileRef = A1E618E31E7934C700D8ED93 /* HealthKitConfigTableViewCell.swift */; };
 		A1EA154D1B01E6EC0052A6E6 /* DatapointTableViewCell.swift in Sources */ = {isa = PBXBuildFile; fileRef = A1EA154C1B01E6EC0052A6E6 /* DatapointTableViewCell.swift */; };
 		A1F9D1EA211B9B7600E2BC93 /* EditDatapointViewController.swift in Sources */ = {isa = PBXBuildFile; fileRef = A1F9D1E9211B9B7600E2BC93 /* EditDatapointViewController.swift */; };
+		E4015D9F2D10DC4D00F58D94 /* SceneDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = E4015D9E2D10DC4D00F58D94 /* SceneDelegate.swift */; };
 		E41286F12A62E6840093D598 /* KeychainSwift in Frameworks */ = {isa = PBXBuildFile; productRef = E41286F02A62E6840093D598 /* KeychainSwift */; };
 		E41286F32A62E97B0093D598 /* KeychainSwift in Frameworks */ = {isa = PBXBuildFile; productRef = E41286F22A62E97B0093D598 /* KeychainSwift */; };
 		E412DADF2B869E1E0099E483 /* BeeLemniscateView.swift in Sources */ = {isa = PBXBuildFile; fileRef = E412DADE2B869E1E0099E483 /* BeeLemniscateView.swift */; };
@@ -246,7 +246,6 @@
 		A196CB181AE4142E00B90A3E /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
 		A196CB191AE4142E00B90A3E /* AppDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = AppDelegate.swift; sourceTree = "<group>"; };
 		A196CB1E1AE4142F00B90A3E /* GalleryViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = GalleryViewController.swift; sourceTree = "<group>"; };
-		A196CB211AE4142F00B90A3E /* Base */ = {isa = PBXFileReference; lastKnownFileType = file.storyboard; name = Base; path = Base.lproj/Main.storyboard; sourceTree = "<group>"; };
 		A196CB231AE4142F00B90A3E /* Images.xcassets */ = {isa = PBXFileReference; lastKnownFileType = folder.assetcatalog; path = Images.xcassets; sourceTree = "<group>"; };
 		A196CB2C1AE4142F00B90A3E /* BeeSwiftTests.xctest */ = {isa = PBXFileReference; explicitFileType = wrapper.cfbundle; includeInIndex = 0; path = BeeSwiftTests.xctest; sourceTree = BUILT_PRODUCTS_DIR; };
 		A196CB311AE4142F00B90A3E /* Info.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = Info.plist; sourceTree = "<group>"; };
@@ -268,6 +267,7 @@
 		A1E618FF1E86980900D8ED93 /* HealthKitConfig.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = HealthKitConfig.swift; sourceTree = "<group>"; };
 		A1EA154C1B01E6EC0052A6E6 /* DatapointTableViewCell.swift */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.swift; path = DatapointTableViewCell.swift; sourceTree = "<group>"; };
 		A1F9D1E9211B9B7600E2BC93 /* EditDatapointViewController.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = EditDatapointViewController.swift; sourceTree = "<group>"; };
+		E4015D9E2D10DC4D00F58D94 /* SceneDelegate.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = SceneDelegate.swift; sourceTree = "<group>"; };
 		E4040D732A7B5F0E008E7D0E /* WorkoutMinutesHealthKitMetric.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = WorkoutMinutesHealthKitMetric.swift; sourceTree = "<group>"; };
 		E41286ED2A62DF330093D598 /* BeeminderModel.xcdatamodel */ = {isa = PBXFileReference; lastKnownFileType = wrapper.xcdatamodel; path = BeeminderModel.xcdatamodel; sourceTree = "<group>"; };
 		E412DADE2B869E1E0099E483 /* BeeLemniscateView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BeeLemniscateView.swift; sourceTree = "<group>"; };
@@ -490,8 +490,8 @@
 				A196CB231AE4142F00B90A3E /* Images.xcassets */,
 				9B8CA57C24B120CA009C86C2 /* LaunchScreen.storyboard */,
 				E43BEA832A036A9C00FC3A38 /* LogReader.swift */,
-				A196CB201AE4142F00B90A3E /* Main.storyboard */,
 				A1453B3E1AEDFCC8006F48DA /* SignInViewController.swift */,
+				E4015D9E2D10DC4D00F58D94 /* SceneDelegate.swift */,
 			);
 			path = BeeSwift;
 			sourceTree = "<group>";
@@ -959,7 +959,6 @@
 			buildActionMask = 2147483647;
 			files = (
 				E5DF493724DC69A200260560 /* Config.swift.sample in Resources */,
-				A196CB221AE4142F00B90A3E /* Main.storyboard in Resources */,
 				9B8CA57D24B120CA009C86C2 /* LaunchScreen.storyboard in Resources */,
 				A196CB241AE4142F00B90A3E /* Images.xcassets in Resources */,
 			);
@@ -1017,6 +1016,7 @@
 				A196CB1F1AE4142F00B90A3E /* GalleryViewController.swift in Sources */,
 				A1BE73AA1E8B45BF00DEC4DB /* ChooseHKMetricViewController.swift in Sources */,
 				A149147B1BE79FD50060600A /* EditNotificationsViewController.swift in Sources */,
+				E4015D9F2D10DC4D00F58D94 /* SceneDelegate.swift in Sources */,
 				A1BE73AC1E8B47E700DEC4DB /* HealthKitMetricTableViewCell.swift in Sources */,
 				E43833942AC1473E0098A38F /* InlineDatePicker.swift in Sources */,
 				E55760F526549D310076B95A /* AddDataIntentHandler.swift in Sources */,
@@ -1176,17 +1176,6 @@
 			targetProxy = E5FEFB3A24E6FAC800A076BB /* PBXContainerItemProxy */;
 		};
 /* End PBXTargetDependency section */
-
-/* Begin PBXVariantGroup section */
-		A196CB201AE4142F00B90A3E /* Main.storyboard */ = {
-			isa = PBXVariantGroup;
-			children = (
-				A196CB211AE4142F00B90A3E /* Base */,
-			);
-			name = Main.storyboard;
-			sourceTree = "<group>";
-		};
-/* End PBXVariantGroup section */
 
 /* Begin XCBuildConfiguration section */
 		A196CB341AE4142F00B90A3E /* Debug */ = {

BeeSwift/Base.lproj/Main.storyboard
@@ -1,53 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.Storyboard.XIB" version="3.0" toolsVersion="20037" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" colorMatched="YES" initialViewController="E0t-UY-3pO">
-    <device id="retina6_1" orientation="portrait" appearance="light"/>
-    <dependencies>
-        <deployment identifier="iOS"/>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="20020"/>
-        <capability name="System colors in document resources" minToolsVersion="11.0"/>
-        <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
-    </dependencies>
-    <scenes>
-        <!--Navigation Controller-->
-        <scene sceneID="qqJ-th-mte">
-            <objects>
-                <navigationController id="E0t-UY-3pO" sceneMemberID="viewController">
-                    <navigationBar key="navigationBar" contentMode="scaleToFill" barStyle="black" translucent="NO" id="Pjd-c5-Ja5">
-                        <rect key="frame" x="0.0" y="44" width="414" height="44"/>
-                        <autoresizingMask key="autoresizingMask"/>
-                        <color key="tintColor" systemColor="labelColor"/>
-                    </navigationBar>
-                    <connections>
-                        <segue destination="61E-DD-3wo" kind="relationship" relationship="rootViewController" id="mRC-E2-iEZ"/>
-                    </connections>
-                </navigationController>
-                <placeholder placeholderIdentifier="IBFirstResponder" id="TF4-3p-N2O" userLabel="First Responder" sceneMemberID="firstResponder"/>
-            </objects>
-            <point key="canvasLocation" x="-1378" y="718"/>
-        </scene>
-        <!--Gallery View Controller-->
-        <scene sceneID="Ylz-8b-nOQ">
-            <objects>
-                <viewController id="61E-DD-3wo" customClass="GalleryViewController" customModule="BeeSwift" customModuleProvider="target" sceneMemberID="viewController">
-                    <layoutGuides>
-                        <viewControllerLayoutGuide type="top" id="vb7-Ub-pCO"/>
-                        <viewControllerLayoutGuide type="bottom" id="Srv-mm-wIg"/>
-                    </layoutGuides>
-                    <view key="view" contentMode="scaleToFill" id="3bJ-hz-njv">
-                        <rect key="frame" x="0.0" y="0.0" width="414" height="808"/>
-                        <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
-                        <color key="backgroundColor" red="1" green="1" blue="1" alpha="1" colorSpace="custom" customColorSpace="sRGB"/>
-                    </view>
-                    <navigationItem key="navigationItem" id="iIR-mA-gmW"/>
-                </viewController>
-                <placeholder placeholderIdentifier="IBFirstResponder" id="C99-1X-rPn" userLabel="First Responder" sceneMemberID="firstResponder"/>
-            </objects>
-            <point key="canvasLocation" x="-660" y="718"/>
-        </scene>
-    </scenes>
-    <resources>
-        <systemColor name="labelColor">
-            <color white="0.0" alpha="1" colorSpace="custom" customColorSpace="genericGamma22GrayColorSpace"/>
-        </systemColor>
-    </resources>
-</document>

BeeSwift/Info.plist
@@ -2,6 +2,23 @@
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
+	<key>UIApplicationSceneManifest</key>
+	<dict>
+		<key>UIApplicationSupportsMultipleScenes</key>
+		<false/>
+		<key>UISceneConfigurations</key>
+		<dict>
+			<key>UIWindowSceneSessionRoleApplication</key>
+			<array>
+				<dict>
+					<key>UISceneDelegateClassName</key>
+					<string>$(PRODUCT_MODULE_NAME).SceneDelegate</string>
+					<key>UISceneConfigurationName</key>
+					<string>Default Configuration</string>
+				</dict>
+			</array>
+		</dict>
+	</dict>
 	<key>BGTaskSchedulerPermittedIdentifiers</key>
 	<array>
 		<string>com.beeminder.beeminder.goals.refresh</string>
@@ -129,8 +146,6 @@
 	</array>
 	<key>UILaunchStoryboardName</key>
 	<string>LaunchScreen</string>
-	<key>UIMainStoryboardFile</key>
-	<string>Main</string>
 	<key>UIRequiredDeviceCapabilities</key>
 	<array>
 		<string>armv7</string>

commit 1f4e9eaf9834b22f4356709b8bcd6d0636d91fcb

goal not opened when tapping spotlight result (#628)
## Summary
Tapping a spotlight result should open not only the beeminder app but
also the corresponding goal in the app.

## Validation
Searched for the name of a goal. Tapped the Spotlight result
corresponding to beeminder. Noted app was then opened to the goal.


This may have worked before and if so, my guess is that it broke with
Commit 758d7fe.
Fixes #626

BeeSwift/AppDelegate.swift
@@ -176,17 +176,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     private func removeAllLocalNotifications() {
         UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
     }
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        if userActivity.activityType == CSSearchableItemActionType {
-            guard let goalIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String else { return false
-            }
-            NotificationCenter.default.post(name: GalleryViewController.NotificationName.openGoal, object: nil, userInfo: ["identifier": goalIdentifier])
-        } else if let intent = userActivity.interaction?.intent as? AddDataIntent {
-            guard let goalSlug = intent.goal else { return false }
-            NotificationCenter.default.post(name: GalleryViewController.NotificationName.openGoal, object: nil, userInfo: ["slug": goalSlug])
-        } else if let goalSlug = userActivity.userInfo?["slug"] {
-            NotificationCenter.default.post(name: GalleryViewController.NotificationName.openGoal, object: nil, userInfo: ["slug": goalSlug])
-        }
-        return true
-    }
 }

BeeSwift/SceneDelegate.swift
@@ -1,17 +1,21 @@
 // Part of BeeSwift. Copyright Beeminder
 
+import CoreSpotlight
 import Foundation
+import OSLog
 import UIKit
 
 import BeeKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    let logger = Logger(subsystem: "com.beeminder.beeminder", category: "SceneDelegate")
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: .all))
-
+        
         let galleryVC = GalleryViewController(
             currentUserManager: ServiceLocator.currentUserManager,
             viewContext: ServiceLocator.persistentContainer.viewContext,
@@ -25,9 +29,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         navigationController.navigationBar.isTranslucent = false
         navigationController.navigationBar.barStyle = .black
         navigationController.navigationBar.tintColor = .white
-
+        
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+    }
+    
+    func scene(_ scene: UIScene, continue userActivity: NSUserActivity) {
+        logger.info("\(#function)")
+        
+        var userInfoOfGoalFromSpotlight: [AnyHashable : Any]? {
+            guard userActivity.activityType == CSSearchableItemActionType else { return nil }
+            guard let goalIdentifier = userActivity.userInfo?[CSSearchableItemActivityIdentifier] as? String else { return nil }
+            logger.info("\(#function): continuing from spotlight result, found goal with identifier: \(goalIdentifier)")
+            return ["identifier": goalIdentifier]
+        }
+        
+        var userInfoOfGoalFromIntent: [AnyHashable : Any]? {
+            guard let intent = userActivity.interaction?.intent as? AddDataIntent else { return nil }
+            guard let goalSlug = intent.goal else { return nil }
+            logger.info("\(#function): continuing from intent, found goal named: \(goalSlug)")
+            return ["slug": goalSlug]
+        }
+        
+        var userInfoOfGoalFrom: [AnyHashable : Any]? {
+            guard let goalname = userActivity.userInfo?["slug"] as? String else { return nil }
+            logger.info("\(#function): continuing, found goal named: \(goalname)")
+            return ["slug": goalname]
+        }
+        
+        if let userInfo = userInfoOfGoalFromSpotlight ?? userInfoOfGoalFromIntent ?? userInfoOfGoalFrom {
+            logger.info("\(#function): opening goal")
+            NotificationCenter.default.post(name: GalleryViewController.NotificationName.openGoal,
+                                            object: nil,
+                                            userInfo: userInfo)
+        }
     }
 }