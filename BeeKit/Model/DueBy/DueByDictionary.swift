6f552ca95ab74f638a4eec2c2f8744d36ffbb3dc
Theo Spears <github@theospears.com>
2025-02-23 20:11:42 -0800

    Follow up adjustments to DueBy model (#638)
    ## Summary
    This is a small follow up to #547 adjusting the naming to be more
    consistent with the rest of the app.
    
    ## Validation
    Verified that the app compiled
    Launched the app in simulator and verified I could view the due by
    information for a few goals.

--- BeeKit/Model/DueBy/DueByDictionary.swift
+++ BeeKit/Model/DueBy/DueByDictionary.swift
@@ -1,4 +1,15 @@
 // Part of BeeSwift. Copyright Beeminder
 
+import SwiftyJSON
+
 /// Delta and Total due by YYYYMMDD daystamp
-public typealias DueByDictionary = [String: BeeminderDueByEntry]
+public typealias DueByDictionary = [String: DueByEntry]
+
+extension DueByDictionary {
+    /// Creates a DueByDictionary from SwiftyJSON
+    /// - Parameter json: JSON object containing due by data
+    /// - Returns: Dictionary mapping daystamps to DueByEntry objects
+    public init(json: JSON) {
+        self = json.dictionaryValue.compactMapValues(DueByEntry.init)
+    }
+}
