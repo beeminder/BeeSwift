e633c6617890a0f63677efc2d5983fa3672743ed
krugerk <4656811+krugerk@users.noreply.github.com>
2025-04-08 11:22:05 +0200

    open goal corresponding to goal of tapped notification
    fixes #41

--- BeeSwift/AppDelegate.swift
+++ BeeSwift/AppDelegate.swift
@@ -41,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         
         NetworkActivityIndicatorManager.shared.isEnabled = true
         
+        UNUserNotificationCenter.current().delegate = self
         NotificationCenter.default.addObserver(self, selector: #selector(self.handleGoalsUpdated), name: GoalManager.NotificationName.goalsUpdated, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.handleUserSignedOut), name: CurrentUserManager.NotificationName.signedOut, object: nil)
 
@@ -165,6 +166,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.list, .banner, .sound, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        logger.notice("\(#function)")
+        guard response.actionIdentifier == UNNotificationDefaultActionIdentifier else {
+            completionHandler()
+            return
+        }
+
+        let userInfo = response.notification.request.content.userInfo
+        guard let slug = userInfo["goalname"] as? String else {
+            logger.error("could not find a goalname in the notification's userInfo: \(userInfo)")
+            completionHandler()
+            return
+        }
+
+        logger.info("found slug: \(slug)")
+        
+        NotificationCenter.default.post(name: GalleryViewController.NotificationName.openGoal,
+                                        object: nil,
+                                        userInfo: ["slug": slug])
+
+        completionHandler()
     }
     
     private func resetStateIfUITesting() {
