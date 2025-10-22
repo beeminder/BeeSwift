9b328683f95e5d650057b0b72369536003f6b8e1
krugerk <4656811+krugerk@users.noreply.github.com>
2025-10-22 09:34:16 +0200

    applied code formatting

--- BeeSwift/Gallery/GalleryViewController.swift
+++ BeeSwift/Gallery/GalleryViewController.swift
@@ -158,12 +158,14 @@ class GalleryViewController: UIViewController {
       self,
       selector: #selector(self.keyboardWillShow),
       name: UIResponder.keyboardWillShowNotification,
-      object: nil)
+      object: nil
+    )
     NotificationCenter.default.addObserver(
       self,
       selector: #selector(self.keyboardWillHide),
       name: UIResponder.keyboardWillHideNotification,
-      object: nil)
+      object: nil
+    )
     self.view.addSubview(self.stackView)
     stackView.snp.makeConstraints { (make) -> Void in
       make.top.left.right.equalToSuperview()
@@ -473,28 +475,21 @@ extension GalleryViewController {
   }
 }
 
-
-private extension GalleryViewController {
-  @objc func keyboardWillShow(_ notification: Notification) {
+extension GalleryViewController {
+  @objc fileprivate func keyboardWillShow(_ notification: Notification) {
     guard self.searchBar.searchTextField.isEditing else { return }
     moveViewWithKeyboard(notification: notification, keyboardWillShow: true)
   }
-  func moveViewWithKeyboard(notification: Notification, keyboardWillShow: Bool) {
+  fileprivate func moveViewWithKeyboard(notification: Notification, keyboardWillShow: Bool) {
     guard let userInfo = notification.userInfo else { return }
-    guard
-      let keyboardDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-    else { return }
-    guard
-      let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt
-    else { return }
+    guard let keyboardDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double else { return }
+    guard let curve = userInfo[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else { return }
     let animationOptions = UIView.AnimationOptions(rawValue: curve)
-    UIView.animate(withDuration: keyboardDuration,
-                   delay: 0,
-                   options: animationOptions) { [weak self] in
+    UIView.animate(withDuration: keyboardDuration, delay: 0, options: animationOptions) { [weak self] in
       self?.view.layoutIfNeeded()
     }
   }
-  @objc func keyboardWillHide(_ notification: Notification) {
+  @objc fileprivate func keyboardWillHide(_ notification: Notification) {
     moveViewWithKeyboard(notification: notification, keyboardWillShow: false)
   }
 }
