68fca5b22dd4c14fc073460b6679e1c646af9d99
krugerk <4656811+krugerk@users.noreply.github.com>
2026-02-07 10:16:14 +0100

    clean code
    the independent parts of the views can be configured in their
    inline configuration closures
    
    whereas setUpView is kept minimal, addressing how the views
    relate to one another

--- BeeSwift/Cells/GoalCollectionViewCell.swift
+++ BeeSwift/Cells/GoalCollectionViewCell.swift
@@ -10,72 +10,103 @@ import BeeKit
 import Foundation
 
 class GoalCollectionViewCell: UICollectionViewCell {
-  private let cardView = CardView()
-  private let thumbnailImageView = GoalImageView(isThumbnail: true)
-  private let titleLabel = BSLabel()
-  private let slugLabel = BSLabel()
-  private let countdownLabel = BSLabel()
-  private let todaytaLabel = BSLabel()
+  private lazy var cardView: CardView = {
+    let view = CardView()
+    view.backgroundColor = .secondarySystemGroupedBackground
+    view.layer.cornerRadius = CardLookConstants.cornerRadius
+    view.layer.shadowColor = UIColor.black.cgColor
+    view.layer.shadowOpacity = 0.1
+    view.layer.shadowRadius = 4
+    view.layer.shadowOffset = CardLookConstants.shadowOffset
+    return view
+  }()
+
+  private lazy var thumbnailImageView: GoalImageView = { return GoalImageView(isThumbnail: true) }()
+
+  private lazy var titleLabel: BSLabel = {
+    let label = BSLabel()
+    label.font = UIFont.beeminder.defaultFontHeavy.withSize(17)
+    label.textColor = .label
+    return label
+  }()
+
+  private lazy var slugLabel: BSLabel = {
+    let label = BSLabel()
+    label.font = UIFont.beeminder.defaultFont.withSize(15)
+    label.textColor = .label
+    return label
+  }()
+
+  private lazy var countdownLabel: BSLabel = {
+    let label = BSLabel()
+    label.font = UIFont.beeminder.defaultBoldFont.withSize(13)
+    label.numberOfLines = 0
+    return label
+  }()
+
+  private lazy var todaytaLabel: BSLabel = {
+    let label = BSLabel()
+    label.textColor = .label
+    label.font = UIFont.beeminder.defaultFont.withSize(14)
+    label.textAlignment = .right
+    label.setContentCompressionResistancePriority(.required, for: .horizontal)
+    return label
+  }()
+
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupView()
   }
+
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     setupView()
   }
+
   private func setupView() {
     self.contentView.backgroundColor = .clear
     self.contentView.addSubview(self.cardView)
-    self.cardView.backgroundColor = .secondarySystemGroupedBackground
-    self.cardView.layer.cornerRadius = CardLookConstants.cornerRadius
-    self.cardView.layer.shadowColor = UIColor.black.cgColor
-    self.cardView.layer.shadowOpacity = 0.1
-    self.cardView.layer.shadowRadius = 4
-    self.cardView.layer.shadowOffset = CardLookConstants.shadowOffset
 
-    self.slugLabel.textColor = .label
     self.cardView.snp.makeConstraints { make in make.edges.equalToSuperview().inset(6) }
+
     [self.thumbnailImageView, self.titleLabel, self.todaytaLabel, self.slugLabel, self.countdownLabel].forEach {
       self.cardView.addSubview($0)
     }
+
     self.thumbnailImageView.snp.makeConstraints { make in
       make.left.top.bottom.equalToSuperview().inset(CardLookConstants.spacing)
       make.width.equalTo(CGFloat(Constants.thumbnailWidth))
       make.height.equalTo(CGFloat(Constants.thumbnailHeight))
     }
-    self.titleLabel.font = UIFont.beeminder.defaultFontHeavy.withSize(17)
-    self.titleLabel.textColor = .label
+
     self.titleLabel.snp.makeConstraints { make in
       make.top.equalToSuperview().offset(CardLookConstants.verticalPadding)
       make.left.equalTo(self.thumbnailImageView.snp.right).offset(CardLookConstants.spacing)
       make.right.equalTo(self.todaytaLabel.snp.left).offset(-8)
     }
-    self.todaytaLabel.textColor = .label
-    self.todaytaLabel.font = UIFont.beeminder.defaultFont.withSize(14)
-    self.todaytaLabel.textAlignment = .right
+
     self.todaytaLabel.snp.makeConstraints { make in
       make.centerY.equalTo(self.titleLabel)
       make.right.equalToSuperview().offset(-CardLookConstants.horizontalPadding)
     }
-    self.todaytaLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
-    self.slugLabel.font = UIFont.beeminder.defaultFont.withSize(15)
+
     self.slugLabel.snp.makeConstraints { make in
       make.top.equalTo(self.titleLabel.snp.bottom).offset(2)
       make.left.right.equalTo(self.titleLabel)
     }
-    self.countdownLabel.font = UIFont.beeminder.defaultBoldFont.withSize(13)
-    self.countdownLabel.numberOfLines = 0
+
     self.countdownLabel.snp.makeConstraints { make in
       make.top.equalTo(self.slugLabel.snp.bottom).offset(4)
       make.left.right.equalTo(self.titleLabel)
       make.bottom.lessThanOrEqualToSuperview().offset(-CardLookConstants.verticalPadding)
     }
   }
+
   override func prepareForReuse() {
     super.prepareForReuse()
     self.configure(with: nil)
   }
+
   func configure(with goal: Goal?) {
     self.thumbnailImageView.goal = goal
     self.titleLabel.text = goal?.title
