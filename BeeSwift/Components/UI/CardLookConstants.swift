e87684239db08499db24c6a3be4fb7d611c6c5c4
krugerk <4656811+krugerk@users.noreply.github.com>
2026-02-07 09:34:42 +0100

    intro cardview

---
+++ BeeSwift/Components/UI/CardLookConstants.swift
@@ -0,0 +1,15 @@
+// Part of BeeSwift. Copyright Beeminder
+
+struct CardLookConstants {
+  static let cornerRadius: CGFloat = 12
+  static let shadowOpacity: Float = 0.08
+  static let shadowRadius: CGFloat = 6
+  static let shadowOffset = CGSize(width: 0, height: 2)
+  static let horizontalPadding: CGFloat = 16
+  static let verticalPadding: CGFloat = 14
+  static let spacing: CGFloat = 12
+  static let primaryBackground = UIColor.secondarySystemBackground
+  static let secondaryBackground = UIColor.tertiarySystemBackground
+  static let borderColor = UIColor.separator
+  static let borderWidth: CGFloat = 0.5
+}
