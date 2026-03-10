da0a09d050315173f08c19585571cf37d63b224a
krugerk <4656811+krugerk@users.noreply.github.com>
2026-03-11 00:04:34 +0100

    clean code

--- BeeSwift/DeeplinkGenerator.swift
+++ BeeSwift/DeeplinkGenerator.swift
@@ -38,22 +38,10 @@ public enum WebEndpoint {
 }
 
 struct DeeplinkGenerator {
-  public static func generateDeepLinkToGoalCommitment(username: String, goalName: String) -> URL {
-    WebEndpoint.goal(username: username, goalName: goalName, tab: .commitment).url
+  public static func webURLToGoal(username: String, goalName: String, tab: GoalTab) -> URL {
+    WebEndpoint.goal(username: username, goalName: goalName, tab: tab).url
   }
-  public static func generateDeepLinkToGoalStop(username: String, goalName: String) -> URL {
-    WebEndpoint.goal(username: username, goalName: goalName, tab: .stop).url
   }
-
-  public static func generateDeepLinkToGoalData(username: String, goalName: String) -> URL {
-    WebEndpoint.goal(username: username, goalName: goalName, tab: .data).url
-  }
-
-  public static func generateDeepLinkToGoalStatistics(username: String, goalName: String) -> URL {
-    WebEndpoint.goal(username: username, goalName: goalName, tab: .statistics).url
-  }
-  public static func generateDeepLinkToGoalSettings(username: String, goalName: String) -> URL {
-    WebEndpoint.goal(username: username, goalName: goalName, tab: .settings).url
   }
   public static func generateDeepLinkToUrl(accessToken: String, username: String, url: URL) -> URL {
     WebEndpoint.apiRedirect(username: username, accessToken: accessToken, redirectTo: url).url
