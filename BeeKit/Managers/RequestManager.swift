e037c8e9a1af0d4ae548311d3c85e93b2cfb9a84
krugerk <4656811+krugerk@users.noreply.github.com>
2026-04-19 14:23:20 +0200

    refactoring away from force unwrap

--- BeeKit/Managers/RequestManager.swift
+++ BeeKit/Managers/RequestManager.swift
@@ -79,23 +79,25 @@ extension RequestManager {
 
 extension RequestManager {
   fileprivate func signedParameters(_ params: [String: Any]?) -> [String: Any]? {
-    if params == nil { return params }
+    guard let params else { return nil }
+    
     var signed = params
     var base = ""
-    var keys = Array(params!.keys)
+    var keys = Array(params.keys)
     keys.sort(by: { $0 < $1 })
     for key in keys {
-      let value: AnyObject? = params![key] as AnyObject?
-      if !(value is String) { return params! }
+      let value: AnyObject? = params[key] as AnyObject?
+      if !(value is String) { return params }
       let allowedCharacterSet = (CharacterSet(charactersIn: "@/").inverted)
       let escapedKey = key.addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)
-      let escapedValue = (params![key] as AnyObject).addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)
+      let escapedValue = (params[key] as AnyObject).addingPercentEncoding(withAllowedCharacters: allowedCharacterSet)
       if base.count > 0 { base += "&" }
       base += "\(escapedKey!)=\(escapedValue!)"
     }
     let token = base.hmac(algorithm: HMACAlgorithm.SHA1, key: Config().requestSigningKey)
-    signed?["beemios_token"] = token
-    return signed! as [String: Any]
+    signed["beemios_token"] = token
+    
+    return signed as [String: Any]
   }
 }
 
